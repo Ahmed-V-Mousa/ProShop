ef338d9a865df6b79baa94803caf1ffe341c230f
Ahmed-V-Mousa <ahmed.v.mousa@gmail.com>
2023-01-07 21:58:26 +0200

    lesson 28 commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-const App = () => {
+function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <main className='py-3'>
+      <main>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
-};
+}
 
 export default App;
